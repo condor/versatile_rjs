de3aaf18cc117c87a00651df7b338295c4821c40
Kevin T <tailto@mac.com>
2011-10-08 16:13:34 +0900

    simplify

--- lib/versatile_rjs/proxy/framework_dependent.rb
+++ lib/versatile_rjs/proxy/framework_dependent.rb
@@ -2,30 +2,31 @@ module VersatileRJS
   class Proxy
     module FrameworkDependent
       def self.included(base)
-        base.instance_eval do
-          class <<self
-            def new_insance(*args)
-              implementation_class.new(*args)
-            end
+        base.extend ClassMethods
+      end
 
-            alias_method :new, :new_instance
+      module ClassMethods
+        def new_insance(*args)
+          implementation_class.new(*args)
+        end
 
-            def implementation_class
-              @implementation_class ||= retrieve_implementation_class
-            end
+        alias_method :new, :new_instance
 
-            def retrieve_implementation_class
-              class_name_tree = name.split('::')
+        private
+        def implementation_class
+          @implementation_class ||= retrieve_implementation_class
+        end
 
-              class_dirnames = class_name_tree[0...-1]
-              class_basename = class_name_tree[-1]
-              framework_module = VersatileRJS.javascript_framework.to_s.camelcase
-              implementation_class_name = [class_dirnames, framework_module, class_basename].flatten.join('::')
+        def retrieve_implementation_class
+          class_name_tree = name.split('::')
 
-              require implementation_class_name.underscore
-              base.implementation_class = implementation_class_name.constantize
-            end
-          end
+          class_dirnames = class_name_tree[0...-1]
+          class_basename = class_name_tree[-1]
+          framework_module = VersatileRJS.javascript_framework.to_s.camelcase
+          implementation_class_name = [class_dirnames, framework_module, class_basename].flatten.join('::')
+
+          require implementation_class_name.underscore
+          base.implementation_class = implementation_class_name.constantize
         end
       end
     end
