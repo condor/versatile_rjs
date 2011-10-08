9e40f0ec4d25a11a7173382fb1b27493eedc7182
Kevin T <tailto@mac.com>
2011-10-08 15:32:59 +0900

    added default javascript_framework as :jquery

--- lib/versatile_rjs.rb
+++ lib/versatile_rjs.rb
@@ -1,8 +1,15 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'versatile_rjs/page'
+require 'versatile_rjs/proxy'
+require 'versatile_rjs/template_handler'
+
 module VersatileRjs
   VERSION = '0.0.1'
 
-  attr_accessor :javascript_framework
+  class <<self
+    attr_accessor :javascript_framework
+  end
+  self.javascript_framework = :jquery
 end
