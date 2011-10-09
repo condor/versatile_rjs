f236c5b2723ce561b9d0f60ce8aaf945cbc1147f
Kevin T <tailto@mac.com>
2011-10-09 16:16:13 +0900

    fixed apprent bug.

--- lib/versatile_rjs/template_handler.rb
+++ lib/versatile_rjs/template_handler.rb
@@ -7,10 +7,8 @@ module VersatileRJS
     def call(template)
       page_procedure = "lambda{|page|" + template.source + "}"
       script =<<-EOF
-      statement = lambda do |page|
-#{template.source}
-        page
-      end.call(VersatileRJS::TemplateHandler.new(self)).to_script
+      statement = lambda{|page|#{template.source}
+        page}.call(VersatileRJS::Page.new(self)).to_script
       statement = "try{" + statement + "}catch(e){alert('" + escape_javascript(statement) + "');throw e;}" if Rails.env.development?
       statement
       EOF
