f2d2b8c6a782b45b217031449e54cb529ea081be
Kevin T <tailto@mac.com>
2011-10-10 08:01:25 +0900

    delegating is not required.

--- lib/versatile_rjs/railtie.rb
+++ lib/versatile_rjs/railtie.rb
@@ -5,7 +5,7 @@ module VersatileRJS
     initializer "versatile_rjs.enable_debug_rjs", :before => "action_view.set_configs" do |app|
       ActiveSupport.on_load(:action_view) do
         class << ActionView::Base
-          superclass_delegating_accessor :debug_rjs
+          attr_accessor :debug_rjs
           self.debug_rjs = false
         end
       end
