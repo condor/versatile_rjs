a69ef0214eacf2c35a2e2bc3122837980d47af86
Kevin T <tailto@mac.com>
2011-10-11 05:11:44 +0900

    removed unneeded sentence.

--- lib/versatile_rjs/template_handler.rb
+++ lib/versatile_rjs/template_handler.rb
@@ -5,7 +5,6 @@ module VersatileRJS
     end
 
     def call(template)
-      page_procedure = "lambda{|page|" + template.source + "}"
       "lambda{|page|#{template.source};page}.call(VersatileRJS::Page.new(self)).to_script(true)"
     end
   end
