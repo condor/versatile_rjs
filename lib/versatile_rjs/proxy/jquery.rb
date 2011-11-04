9a92b0b9e25ce9d4549ca17ef6ffa86e1b4ea105
Kevin T <tailto@mac.com>
2011-11-04 16:39:39 +0900

    Separate framework-dependent requision into a ruby file.

---
+++ lib/versatile_rjs/proxy/jquery.rb
@@ -0,0 +1,4 @@
+require 'versatile_rjs/proxy/jquery/element_proxy'
+require 'versatile_rjs/proxy/jquery/element_by_id_proxy'
+require 'versatile_rjs/proxy/jquery/element_set_proxy'
+require 'versatile_rjs/proxy/jquery/selector_proxy'
