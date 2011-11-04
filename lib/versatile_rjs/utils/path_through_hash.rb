3c47b42650c258673fc73897320981a1daf63aa4
Kevin T <tailto@mac.com>
2011-11-04 16:34:59 +0900

    Add PathThroughHash.

---
+++ lib/versatile_rjs/utils/path_through_hash.rb
@@ -0,0 +1,11 @@
+module VersatileRJS
+  module Utils
+    class PathThroughHash < Hash
+      def initialize(hash = {})
+        super{|h, k|k}
+        merge!(hash)
+        freeze
+      end
+    end
+  end
+end
