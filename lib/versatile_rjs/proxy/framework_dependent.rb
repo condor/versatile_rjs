140d0f60718fa28fa16ff7a3ffd4234ae8f3d06a
Kevin T <tailto@mac.com>
2011-11-04 11:36:37 +0900

    Delegated retrieval of implementation_class to VersatileRJS

--- lib/versatile_rjs/proxy/framework_dependent.rb
+++ lib/versatile_rjs/proxy/framework_dependent.rb
@@ -12,20 +12,9 @@ module VersatileRJS
 
         private
         def implementation_class
-          @implementation_class ||= retrieve_implementation_class
+          @implementation_class ||= VersatileRJS.implementation_class_of(self)
         end
 
-        def retrieve_implementation_class
-          class_name_tree = name.split('::')
-
-          class_dirnames = class_name_tree[0...-1]
-          class_basename = class_name_tree[-1]
-          framework_module = VersatileRJS.javascript_framework.to_s.camelcase
-          implementation_class_name = [class_dirnames, framework_module, class_basename].flatten.join('::')
-
-          require implementation_class_name.underscore
-          implementation_class_name.constantize
-        end
       end
     end
   end
