fec1dc29975ded0c8015cf7c72e059a4cf42efd6
Kevin T <tailto@mac.com>
2011-11-04 11:35:46 +0900

    Add alias debug_rjs? / implementation_class_of method

--- lib/versatile_rjs.rb
+++ lib/versatile_rjs.rb
@@ -7,9 +7,24 @@ require 'versatile_rjs/template_handler'
 require 'versatile_rjs/railtie'
 
 module VersatileRJS
-  VERSION = '0.0.2'
+  VERSION = '0.1.0'
 
   class <<self
     attr_accessor :javascript_framework, :debug_rjs
+    alias_method :debug_rjs?, :debug_rjs
+  end
+
+  def self.framework_module
+    javascript_framework.to_s.camelcase
+  end
+
+  def self.implementation_class_of(mod)
+    class_name_tree = mod.name.split('::')
+
+    class_dirnames = class_name_tree[0...-1]
+    class_basename = class_name_tree[-1]
+    implementation_class_name = [class_dirnames, framework_module, class_basename].flatten.join('::')
+
+    implementation_class_name.constantize
   end
 end
