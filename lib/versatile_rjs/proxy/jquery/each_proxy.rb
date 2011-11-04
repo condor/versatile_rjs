f91379a238a1eab1b103ca6b05e7a0903a014a9e
Kevin T <tailto@mac.com>
2011-11-04 17:20:23 +0900

    implemented EachProxy for JQuery

--- lib/versatile_rjs/proxy/jquery/each_proxy.rb
+++ lib/versatile_rjs/proxy/jquery/each_proxy.rb
@@ -2,9 +2,18 @@ module VersatileRJS
   class Proxy
     module Jquery
       class EachProxy < VersatileRJS::Proxy::EachProxy
+        def initialize(page)
+          super
+          add_statement(ActiveSupport::JSON::Variable.new("var #{variable_name} = $(#{tmp_variable_name})"))
+        end
+
+        def tmp_variable_name
+          @tmp_variable_name ||= "_tmp" + variable_name
+        end
+
         def statement_prefix(statement)
           loop_variable_name = "_loop" + variable_name
-          "$.each(function(#{loop_variable_name}, #{variable_name}){"
+          "$.each(function(#{loop_variable_name}, #{tmp_variable_name}){"
         end
 
         def statement_suffix(statement)
