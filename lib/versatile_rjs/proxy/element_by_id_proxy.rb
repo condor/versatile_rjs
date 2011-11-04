4f7836ebd7570f780242ce8c9fa00b224998d262
Kevin T <tailto@mac.com>
2011-11-04 11:37:47 +0900

    modulize.

--- lib/versatile_rjs/proxy/element_by_id_proxy.rb
+++ lib/versatile_rjs/proxy/element_by_id_proxy.rb
@@ -2,22 +2,18 @@ require 'versatile_rjs/proxy/element_proxy'
 
 module VersatileRJS
   class Proxy
-    class ElementByIdProxy < ElementProxy
+    module ElementByIdProxy
+      include FrameworkDependent
 
-      attr_reader :id
-
-      def self.inherited(derived)
-        derived.class_eval do
-          def initialize(page, id)
-            super(page, id, statement_by_id(id))
-          end
+      def self.included(base)
+        base.class_eval do
+          attr_accessor :id
         end
-        derived.instance_eval{def inherited(*args);end}
       end
 
-      def initialize(page, id, statement)
-        super(page, statement)
-        @id = id
+      def initialize(page, id)
+        self.id = id
+        super(page, statement_for_id(id))
       end
     end
   end
