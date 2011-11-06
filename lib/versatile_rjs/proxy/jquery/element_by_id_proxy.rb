80964b36718b7cf46bc1425df3b03c79f3b964f4
Kevin T <tailto@mac.com>
2011-11-06 18:00:25 +0900

    Add inclusion

--- lib/versatile_rjs/proxy/jquery/element_by_id_proxy.rb
+++ lib/versatile_rjs/proxy/jquery/element_by_id_proxy.rb
@@ -2,6 +2,7 @@ module VersatileRJS
   class Proxy
     module Jquery
       class ElementByIdProxy < ElementProxy
+        include ::VersatileRJS::Proxy::ElementByIdProxy
         private
         def statement_for_id(id)
           "$('##{id}')"
