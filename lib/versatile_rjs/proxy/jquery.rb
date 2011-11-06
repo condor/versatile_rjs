bc6ef0797c2b12318f22f4d8c1fafdfd6108aabe
Kevin T <tailto@mac.com>
2011-11-06 10:40:23 +0900

    declaring Jquery as module.

--- lib/versatile_rjs/proxy/jquery.rb
+++ lib/versatile_rjs/proxy/jquery.rb
@@ -1,3 +1,8 @@
+module VersatileRJS
+  class Proxy
+    module Jquery;end
+  end
+end
 require 'versatile_rjs/proxy/jquery/element_proxy'
 require 'versatile_rjs/proxy/jquery/element_by_id_proxy'
 require 'versatile_rjs/proxy/jquery/element_set_proxy'
