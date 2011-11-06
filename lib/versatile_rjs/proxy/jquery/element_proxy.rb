0c7d4816e3a4507f38fb9423fe8c60c275d53cd9
Kevin T <tailto@mac.com>
2011-11-06 17:06:25 +0900

    Fixed malimplementation

--- lib/versatile_rjs/proxy/jquery/element_proxy.rb
+++ lib/versatile_rjs/proxy/jquery/element_proxy.rb
@@ -2,41 +2,34 @@ module VersatileRJS
   class Proxy
     module Jquery
       class ElementProxy < VersatileRJS::Proxy::ElementProxy
-        def self.included(base)
-          base.class_eval do
-            METHODS = {
-              :replace_html => :html,
-              :replace => :replaceWith,
-              :insert_html => {
-                :top => :prepend,
-                :bottom => :append
-              },
-              :value => :val,
-              :value= => :val,
-              :select => :find
-            }.freeze
-          end
-          base.extend ClassMethods
+        METHODS = {
+          :replace_html => :html,
+          :replace => :replaceWith,
+          :insert_html => {
+            :top => :prepend,
+            :bottom => :append
+          },
+          :value => :val,
+          :value= => :val,
+          :select => :find
+        }.freeze
+      end
+
+      def self.js_methods(method, *args)
+        case method
+        when :insert_html
+          METHODS[method][args.first]
+        else
+          METHODS[method]
         end
+      end
 
-        module ClassMethods
-          def js_methods(method, *args)
-            case method
-            when :insert_html
-              METHODS[method][args.first]
-            else
-              METHODS[method]
-            end
-          end
-
-          def args(method, *args)
-            case method
-            when :insert_html
-              args.second
-            else
-              args
-            end
-          end
+      def self.args(method, *args)
+        case method
+        when :insert_html
+          args.second
+        else
+          args
         end
       end
     end
