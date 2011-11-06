55b4744aea5059a79dfdd384ff0cae35f14eea9c
Kevin T <tailto@mac.com>
2011-11-06 16:59:30 +0900

    updated.

--- lib/versatile_rjs/proxy/jquery/element_proxy.rb
+++ lib/versatile_rjs/proxy/jquery/element_proxy.rb
@@ -2,31 +2,40 @@ module VersatileRJS
   class Proxy
     module Jquery
       class ElementProxy < VersatileRJS::Proxy::ElementProxy
-        METHODS = {
-          :replace_html => :html,
-          :replace => :replaceWith,
-          :insert_html => {
-            :top => :prepend,
-            :bottom => :append
-          },
-          :value => :val,
-          :value= => :val,
-          :select => :find
-        }.freeze
-
-        def self.js_methods(method, *args)
-          case method
-          when :insert_html
-            METHODS[method][args.first]
-          else
-            METHODS[method]
+        def self.included(base)
+          base.class_eval do
+            METHODS = {
+              :replace_html => :html,
+              :replace => :replaceWith,
+              :insert_html => {
+                :top => :prepend,
+                :bottom => :append
+              },
+              :value => :val,
+              :value= => :val,
+              :select => :find
+            }.freeze
           end
+          base.extend ClassMethods
         end
 
-        def self.args(method, *args)
-          case method
-          when :insert_html
-            content
+        module ClassMethods
+          def js_methods(method, *args)
+            case method
+            when :insert_html
+              METHODS[method][args.first]
+            else
+              METHODS[method]
+            end
+          end
+
+          def args(method, *args)
+            case method
+            when :insert_html
+              args.second
+            else
+              args
+            end
           end
         end
       end
