fc2931e4b6a3f50dd50d9171bdf1c2344112370d
Naoto "Kevin" IMAI TOYODA <imai.naoto@future.co.jp>
2012-05-12 16:16:38 +0900

    To test the gem with cuke, add new rails app.

---
+++ features/apps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apps::Application.config.secret_token = '9e49dad5c8cc15e7250606d63d4a4ca7e0848c0140a1e8eda55e2ab5fbbf80a6b0f6d73baa850d3bc1933b5efcb9b731384a9fbb022c8d094a893b9f454b1460'
